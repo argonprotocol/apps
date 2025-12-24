c9d58cf84caeed4a00028ae32933bb9bb9f4966b
Blake Byrnes <blakebyrnes@gmail.com>
2025-12-24 12:08:51 -0500

    feat: improve installer speed
    This change hashes docker configs so it can skip force rebuild unless absolutely needed.
    
    As part of this, also kill a hung installer if running on a previous version, and use the installer lock as the pid source, not pgrep.
    
    Finally, the bitcoin docker now mounts the bitcoin configs instead of embedding them so that we don’t need to detect changes, and the miner removes it’s unneeded docker.

--- server/bitcoin/entrypoint.sh
+++ server/bitcoin/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -d /data ]; then
-    echo "Make sure data directory exits already for this to work"
+    echo "Make sure data directory exists already for this to work"
     exit 1
 fi
 
